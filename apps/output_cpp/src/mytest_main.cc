003fdb5d18c2e15fc0a21fff01b289e2d74c74b8
Yiwei Zhao <2294622497@qq.com>
2019-09-23 14:10:30 +0800

    111

--- apps/output_cpp/src/mytest_main.cc
+++ apps/output_cpp/src/mytest_main.cc
@@ -1,5 +1,125 @@
 #include "mytest.h"
-int main(int argc, char** argv) {
+#include "common_main.h"
+
+class my_main: public main_t
+{
+public:
+    node_t*  parent;
+    long* pid;
+    long* id;
+    int* count;
+    long size;
+    int* star;
+
+    virtual ~my_main() {
+        delete[] parent;
+        delete[] pid;
+        delete[] id;
+        delete[] count;
+        delete[] star;
+        delete[] membership;
+    }
+
+    my_main(){
+    }
+
+    virtual bool prepare() {
+        node_t*  parent = new node_t[G.num_nodes()+1];
+        long* pid = new long[G.num_nodes()+1];
+        long* id = new long[G.num_nodes()+1];
+        int* count = new int[G.num_nodes()+1];
+        long size = G.num_nodes();
+        int* star = new int[G.num_nodes()+1];
+        return true;
+    }
+
+    virtual bool run()
+    {
+        printf("Graph has %d node\n",G.num_nodes());
+        for (int i = 0; i < G.num_nodes(); i++)
+        {
+            id[i] = i;
+            pid[i] = i;
+
+            count[i] = 0;
+            star[i] = 0;
+        }
+
+
+        init(G, parent,id,pid);
+        mistake(G,parent,id,pid);
+        for(int i =0;i<G.num_nodes();i++){
+            printf("%d's parent = %ld\n",i,pid[i]);
+        }
+        singleton_elimination(G,size,parent,id,pid,count);
+        for(int i =0;i<G.num_nodes();i++){
+            printf("%d's son count = %ld\n",i,count[i]);
+        }
+        for(int i =0;i<G.num_nodes();i++){
+            printf("%d's parent = %ld\n",i,pid[i]);
+        }
+        long ret1 = star_detection(G,parent,id,pid,star);
+        printf("%ld \n",ret1);
+        for(int i =0;i<G.num_nodes();i++){
+            printf("%d's star = %ld\n",i,star[i]);
+        }
+        int ii = 0;
+        //avoid while(true)
+        while(ii++ < 100000){
+            C_star_hooking(G,size,parent,id,pid,star);
+            U_star_hooking(G,size,parent,id,pid,star);
+
+            for(int i =0;i<G.num_nodes();i++){
+                node_t fn = parent[i];
+                printf("%d's parent = %ld = %d\n",i,pid[i],fn);
+            }
+
+            for(int i =0;i<G.num_nodes();i++){
+                printf("%d's star = %ld\n",i,star[i]);
+            }
+
+            pointer_jumping(G,parent,id,pid);
+
+            for(int i =0;i<G.num_nodes();i++){
+                node_t fn = parent[i];
+                printf("%d's parent = %ld = %d\n",i,pid[i],fn);
+            }
+
+            for(int i =0;i<G.num_nodes();i++){
+                printf("%d's star = %ld\n",i,star[i]);
+                star[i] = 0;
+            }
+
+            long ret1 = star_detection(G,parent,id,pid,star);
+
+            printf("%ld \n",ret1);
+            for(int i =0;i<G.num_nodes();i++){
+                printf("%d's star = %ld\n",i,star[i]);
+            }
+
+            if(ret1 == size)
+                break;
+        }
+        return true;
+    }
+
+    virtual bool post_process() {
+        for(int i =0;i<G.num_nodes();i++){
+            node_t fn = parent[i];
+            printf("%d's parent = %ld = %d\n",i,pid[i],fn);
+        }
+        return true;
+    }
+};
+
+#define TEST_LARGE  1
+
+int main(int argc, char** argv)
+{
+#if TEST_LARGE
+    my_main M;
+    M.main(argc, argv);
+#else
     gm_graph G;
     for(int i=0;i<=19;i++){
         G.add_node();
@@ -25,7 +145,7 @@ int main(int argc, char** argv) {
         {
 	   id[i] = i;
            pid[i] = i;
-	 
+
 	   count[i] = 0;
 	   star[i] = 0;
 	}
@@ -33,19 +153,19 @@ int main(int argc, char** argv) {
 
      init(G, parent,id,pid);
      mistake(G,parent,id,pid);
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's parent = %ld\n",i,pid[i]);
      }
      singleton_elimination(G,size,parent,id,pid,count);
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's son count = %ld\n",i,count[i]);
      }
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's parent = %ld\n",i,pid[i]);
      }
     long ret1 = star_detection(G,parent,id,pid,star);
-    printf("%ld \n",ret1);  
-     for(int i =1;i<G.num_nodes();i++){
+    printf("%ld \n",ret1);
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's star = %ld\n",i,star[i]);
      }
     int ii = 0;
@@ -54,23 +174,23 @@ int main(int argc, char** argv) {
      C_star_hooking(G,size,parent,id,pid,star);
      U_star_hooking(G,size,parent,id,pid,star);
 
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
 	    node_t fn = parent[i];
             printf("%d's parent = %ld = %d\n",i,pid[i],fn);
      }
 
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's star = %ld\n",i,star[i]);
      }
 
      pointer_jumping(G,parent,id,pid);
 
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
 	    node_t fn = parent[i];
             printf("%d's parent = %ld = %d\n",i,pid[i],fn);
      }
 
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's star = %ld\n",i,star[i]);
 	    star[i] = 0;
      }
@@ -78,11 +198,12 @@ int main(int argc, char** argv) {
      long ret1 = star_detection(G,parent,id,pid,star);
 
      printf("%ld \n",ret1);
-     for(int i =1;i<G.num_nodes();i++){
+     for(int i =0;i<G.num_nodes();i++){
             printf("%d's star = %ld\n",i,star[i]);
      }
 
      if(ret1 == size)
 	    break;
    }
+#endif
 }
