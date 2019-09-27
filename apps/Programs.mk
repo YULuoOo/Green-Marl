b471151e5a201b8e5bbe1336b25a883b54620ab3
root <2294622497@qq.com>
2019-09-27 23:35:03 +0800

    mst_prim

--- apps/Programs.mk
+++ apps/Programs.mk
@@ -1,11 +1,11 @@
 ifndef PROGS
  ifeq ($(TARGET), gps)
-   PROGS=naive_cc ytest pagerank avg_teen_cnt conduct hop_dist sssp random_bipartite_matching bc_random
+   PROGS=mst_prim naive_cc ytest pagerank avg_teen_cnt conduct hop_dist sssp random_bipartite_matching bc_random
  else
   ifeq ($(TARGET), giraph)
-    PROGS= naive_cc mytest pagerank avg_teen_cnt conduct hop_dist sssp random_bipartite_matching bc_random triangle_counting_directed parallel_random_walk_jump_sampling
+    PROGS= mst_prim naive_cc mytest pagerank avg_teen_cnt conduct hop_dist sssp random_bipartite_matching bc_random triangle_counting_directed parallel_random_walk_jump_sampling
   else
-    PROGS= naive_cc mytest sssp_path hop_dist communities bc_random triangle_counting potential_friends pagerank avg_teen_cnt conduct bc kosaraju adamicAdar v_cover sssp random_walk_sampling_with_random_jump random_degree_node_sampling random_node_sampling sssp_path_adj bc_adj sssp_dijkstra bidir_dijkstra
+    PROGS= mst_prim naive_cc mytest sssp_path hop_dist communities bc_random triangle_counting potential_friends pagerank avg_teen_cnt conduct bc kosaraju adamicAdar v_cover sssp random_walk_sampling_with_random_jump random_degree_node_sampling random_node_sampling sssp_path_adj bc_adj sssp_dijkstra bidir_dijkstra
   endif
  endif
 endif
