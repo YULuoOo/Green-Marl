ad3519a6ca3a0c31eb6e55fc2e7dd4c28357e991
Yiwei Zhao <2294622497@qq.com>
2019-10-15 21:52:06 +0800

    no message

--- apps/output_cpp/src/fwbw_scc_main.cc
+++ apps/output_cpp/src/fwbw_scc_main.cc
@@ -7,27 +7,31 @@ class my_main: public main_t
 {
 public:
     int* scc;
-    int* state;
+    int* color;
+    int* id;
+
 
     virtual ~my_main() {
         delete[] scc;
-        delete[] state;
-
+        delete[] color;
+        delete[] id;
     }
 
-    my_main() : scc(NULL),state(NULL) {
+    my_main() : scc(NULL),color(NULL),id(NULL) {
     }
 
     virtual bool prepare() {
         printf("prepare");
         scc = new int[G.num_nodes()];
-        state = new int[G.num_nodes()];
+        color = new int[G.num_nodes()];
+        id = new int[G.num_nodes()];
 
 #pragma omp parallel for
         for (int i = 0; i < G.num_nodes(); i++)
         {
             scc[i] = 0;
-            state[i] = 0;
+            color[i] = i;
+            id[i] = i;
         }
         printf("Graph has %d node\n",G.num_nodes());
         return true;
@@ -35,8 +39,8 @@ public:
 
     virtual bool run()
     {
-        long rettt = opt_cc(G,size,parent,id,pid,star,count);
-        printf("ret = %ld",rettt);
+        Int ret = scc(G,color,id,scc,G.num_nodes());
+        printf("ret = %d",rettt);
         return true;
     }
 
